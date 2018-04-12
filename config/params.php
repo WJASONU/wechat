7cea3cef3fe4dd8137c9c7adb11351060ac87eae
WJASONU <1030989285@qq.com>
2018-04-12 14:54:27 +0800

    1

--- config/params.php
+++ config/params.php
@@ -4,15 +4,7 @@ return [
     'weixin' => [
 		'appid' => 'wx2fa793767bec871c',
 		'sk' => '05524873a9bdf8cce8e1f3222bec6e24',
-		'token' => 'weixin',
-		'aeskey' => '',
-		'pay' => [
-			'key' => '根据实际情况填写',
-			'mch_id' => '根据实际情况填写',
-			'notify_url' => [
-				'm' => '/pay/callback'
-			]
-		]
+		'token' => 'weixin'	
 	]
 ];
 
